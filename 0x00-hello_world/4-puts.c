900fdbb0535db7d1841ce7431e3d87cec542ade2
Ebere-ctrl <iamstanley59@gmail.com>
2023-02-17 15:33:58 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+
 /**
  * main - Entry point
  *
- * Return : Always 0 (Success)
+ * Description: 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
